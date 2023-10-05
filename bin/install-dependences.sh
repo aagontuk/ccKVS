ecb62869d36c70a1b2b6e6d392848445322890ec
Ashfaqur Rahaman <ashfaq@cs.utah.edu>
2023-10-04 20:03:11 -0600

    Updated script for installing gcc-6

--- bin/install-dependences.sh
+++ bin/install-dependences.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+sudo apt update
 sudo apt-get --yes --force-yes install \
    git make gcc numactl libnuma-dev \
    libmemcached-dev zlib1g-dev memcached \
-   libmemcached-dev libmemcached-tools libpapi-dev 
+   libmemcached-dev libmemcached-tools libpapi-dev \
+   libgsl-dev
+
 
 wget https://github.com/ivmai/libatomic_ops/releases/download/v7.4.6/libatomic_ops-7.4.6.tar.gz
 
@@ -19,3 +22,11 @@ sudo make install
 cd ..
 
 rm -rf libatomic_ops-7.4.6*
+
+# Install and activate gcc-6
+sudo add-apt-repository 'deb http://us.archive.ubuntu.com/ubuntu/ bionic main universe'
+sudo apt update
+sudo apt install -y gcc-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 40
+sudo update-alternatives --set gcc /usr/bin/gcc-6
